4a8322097cce8405cb4a5e56406788466c2d05bf
Gustavo Torregrosa Costa <gustavo@Gustavos-MacBook-Air.local>
2023-02-27 10:08:58 -0300

    parcial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
           <p>
             Get started by editing&nbsp;
 
-            Testing GH Actions 1234567
+            Testing GH Actions 1234567 - Felipe
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
           <div>
